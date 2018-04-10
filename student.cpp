000d75b40a1c762224dbc863d724ca8ca7846ef6
thanat121 <35612459+thanat121@users.noreply.github.com>
2018-04-09 20:04:06 -0700

    Update and rename README.md to student.cpp

---
+++ student.cpp
@@ -0,0 +1,39 @@
+# Assignment1
+
+#include "StudentDemo.h"
+StudentDemo::StudentDemo(void)
+{
+}
+StudentDemo::~StudentDemo(void)
+{
+}
+
+#include <iostream>
+#include "Student.h"
+using namespace std;
+
+int main()
+{
+    Student s;
+    int ID;
+    string name;
+    double GPA; 
+    char gender;
+
+    cout << "Enter ID ";
+    cin >> ID;
+    cout << "Enter name ";
+    cin >> name;
+    cout << "Enter GPA ";
+    cin >> GPA;
+    cout << "Enter gender ";
+    cin >> gender;
+    s.setStudent(ID, name, GPA, gender);
+   
+	cout << "Student ID:" << s.getID() <<endl;
+	cout << "Student name:" << s.getName() <<endl;
+	cout << "Student GPA:" << s.getGPA() <<endl;
+	cout << "Student gender:" << s.getGender() <<endl;
+	system("pause");
+    return 0;
+}
